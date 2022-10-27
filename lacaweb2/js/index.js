285180af4df794e05ea1aadb7144d5fb2ab0a975
nehuenortega <87040088+nehuenortega@users.noreply.github.com>
2022-10-26 22:26:55 -0300

    Delete lacaweb2 directory

--- lacaweb2/js/index.js
+++
@@ -1,8 +0,0 @@
-const PORTADA_BTN = document.getElementById('boton-portada')
-const ELEMENT = document.querySelector('#categoria-productos')
-PORTADA_BTN.addEventListener('click', (event) => {
-    event.preventDefault()
-    event.stopPropagation()
-    window.location.href = 'informacion.html';
-    
-});
